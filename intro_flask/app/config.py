afb731ad00bd6950540a0806ade2fc487baad40c
Arthur-codeX <daniel.karanja239@gmail.com>
2025-09-11 09:58:42 +0300

    updated migrations

--- intro_flask/app/config.py
+++ intro_flask/app/config.py
@@ -5,6 +5,9 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+print("ENV DB URL")
+print(os.getenv("DATABASE_URL"))
+
 class Config:
     SQLALCHEMY_DATABASE_URI=os.getenv("DATABASE_URL")
     SQLACHEMY_TRACK_MODIFICATIONS=False
